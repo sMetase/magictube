3bd21c79494f3195fff484d17fdbb16d8e1ae6ba
sMetase <88589894+sMetase@users.noreply.github.com>
2022-06-27 19:03:51 +0800

    Add files via upload

--- Magictube.py
+++ Magictube.py
@@ -1,29 +1,67 @@
+#Magictube.py
+#Created by LC
+#2022/5/21
+
 import pandas as pd
 import os
 
-try:
-    path = str(input("Please enter your folder location.\n\t"))
-    saveloc = str(input("Please enter the location where you want to save the file.\n\t"))
-    x = int(input("Please enter the ending row of the table header\n\t"))
-    y = bool(input("Is the first line a unit? 1/0\n\t"))
+def merge():
+    try:
+        path = str(input("Please enter your folder location.\n\t"))
+        saveloc = str(input("Please enter the location where you want to save the file.\n\t"))
+        x = int(input("Please enter the ending row of the table header\n\t"))
+        y = bool(input("Is the first line a unit? 1/0\n\t"))
 
-except IOError:
-    print("Invalid input.")
+    except IOError:
+        print("Invalid input.")
 
-else:
-    for rootdir, subdir, files in os.walk(path):
-        print('\nWe are scanning folder: %s' %rootdir)
-        print("The files in this folder are as follows:")
-        savepath = os.path.join(saveloc,'%s.xlsx' %rootdir.split("\\")[-1])
-        file_list = pd.DataFrame()
-        for file in files:
-            df_txt = pd.read_csv(os.path.join(rootdir,file), skiprows=x, on_bad_lines='skip', sep="\t") #读取文件
-            print("\t%s" %file) #列出文件
-            if y: #如果首行是单位则去掉首行
-                df_txt.drop(axis=0, index=0, inplace=True)
-            df_txt = df_txt.astype(float)
-            file_list = pd.concat([file_list,df_txt])
-        if file_list.empty == False:
-            file_list.to_excel(savepath, index=False)
-            print("These files that mentioned above are merging into one table %s" %savepath)
-    print("\nAll done!")
+    else:
+        for rootdir, subdir, files in os.walk(path):
+            print('\nWe are scanning folder: %s' %rootdir)
+            print("The files in this folder are as follows:")
+            savepath = os.path.join(saveloc,'%s.xlsx' %rootdir.split("\\")[-1])
+            file_list = pd.DataFrame()
+            for file in files:
+                df_txt = pd.read_csv(os.path.join(rootdir,file), skiprows=x, on_bad_lines='skip', sep="\t") #读取文件
+                print("\t%s" %file) #列出文件
+                if y: #如果首行是单位则去掉首行
+                    df_txt.drop(axis=0, index=0, inplace=True)
+                df_txt = df_txt.astype(float)
+                file_list = pd.concat([file_list,df_txt])
+            if file_list.empty == False:
+                file_list.to_excel(savepath, index=False)
+                print("These files that mentioned above are merging into one table %s" %savepath)
+        print("\nAll done!")
+
+def delete():
+    try:
+        path = str(input("Please enter your folder location.\n\t"))
+        saveloc = str(input("Please enter the location where you want to save the file.\n\t"))
+        x = int(input("Please enter the ending row of the table header\n\t"))
+    
+    except IOError:
+        print("Invalid input.")
+    
+    else:
+        for rootdir, subdir, files in os.walk(path):
+            print('\nWe are scanning folder: %s' %rootdir)
+            print("The files in this folder are as follows:")
+            for file in files:
+                openpath = os.path.join(path,file)
+                savepath = os.path.join(saveloc,file)
+                print('\t%s'%file)
+                with open(openpath,'r') as f:
+                    data = f.readlines()
+                    del data[0:x]
+                    f_new = open(savepath, 'w')
+                    f_new.writelines(data)
+                    f_new.close()
+            print("These files that mentioned above are processing.")
+            print("All done!")
+
+if __name__ == '__main__':
+    modes = input("What do you want, merging or deleting? 1/0\n")
+    if modes == '1':
+        merge()
+    elif modes == '0':
+        delete()
--- Rename.py
+++ Rename.py
@@ -1,12 +1,16 @@
+#Rename.py
+#Created by LC
+#2022/5/21
+
 import os
 co = '-'
 
-
 def rename():
-    path = r"D:\Creep_Recovery"
+    path = str(input("Please enter your folder location.\n\t"))
+    saveloc = str(input("Please enter the location where you want to save the file.\n\t"))
     for rootdir, subdir, files in os.walk(path):
         print("\nWe are scanning folder: %s" %rootdir)
-        dirpath = os.path.join(r'D:\GCreep_Recovery', rootdir.split('\\')[-1])
+        dirpath = os.path.join(saveloc, rootdir.split('\\')[-1])
         if not os.path.exists(dirpath):
             os.makedirs(dirpath)
             print("We created a folder named %s" %dirpath)
